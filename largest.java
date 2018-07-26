da03f0cd7801dbcfcaee38313bae6cccaab4347e
anusha-garre <41730901+anusha-garre@users.noreply.github.com>
2018-07-26 21:51:20 +0530

    Create largest.java

---
+++ largest.java
@@ -0,0 +1,28 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Ideone
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	  int x, y, z;
+      System.out.println("Enter three integers ");
+      Scanner sc = new Scanner(System.in);
+ 
+      x = sc.nextInt();
+      y = sc.nextInt();
+      z = sc.nextInt();
+ 
+      if ( x > y && x > z )
+         System.out.println(x);
+      else if ( y > x && y > z )
+         System.out.println(y);
+      else if ( z > x && z > y )
+         System.out.println(z);
+      else   
+         System.out.println("Entered numbers are not distinct.");
+   }
+}	// your code goes here
+	
